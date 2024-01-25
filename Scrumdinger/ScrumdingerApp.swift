24306592ab801bea5db15fef4e68afab8f24b39c
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-01-25 09:19:38 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jackson Cuevas on 25/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
