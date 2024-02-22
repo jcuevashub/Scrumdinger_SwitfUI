ca761226adc9a54f892f654f1e730d26b29101b3
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-02-22 10:44:44 -0400

    Add an error wrapper structure

---
+++ Scrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,19 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by Jackson Cuevas on 22/2/24.
+//
+
+import Foundation
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+    
+    init(id: UUID, error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
